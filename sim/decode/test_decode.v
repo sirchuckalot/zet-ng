c612a83e5b6363b229dce0bf56753b0c65828d21
sir_chuckalot <charleypicker@yahoo.com>
2014-03-15 22:06:31 -0700

    Improved test readability Displaying all decoded signals

--- sim/decode/test_decode.v
+++ sim/decode/test_decode.v
@@ -19,6 +19,8 @@
 
 `define ENABLE_VCD
 
+`include "defines.v"
+
 module test_decode();
 
 // Testbench regs and wires
@@ -31,6 +33,41 @@ reg rst;
 reg [7:0] instruction;
 reg instruction_valid;
 wire instruction_next;
+
+// current cs and ip of instruction
+wire [15:0] instruction_cs_o;
+wire [15:0] instruction_ip_o;
+
+// {prefix_decoded, repe_repz, repne_repnz, lock}
+wire [3:0] inst_pr_o;
+// {prefix_decoded, x, x, address size override}
+wire [3:0] adrs_pr_o;
+// {prefix_decoded, x, x, operand size override}
+wire [3:0] oper_pr_o;
+// {prefix_decoded, segment override register}
+wire [3:0] sovr_pr_o;
+
+wire [7:0] opcode_o;
+
+wire need_modrm_o;
+wire [7:0] modrm_o;
+
+wire need_off_o;
+wire off_size_o;
+wire [15:0] offset_o;
+
+wire need_imm_o;
+wire imm_size_o;
+wire [15:0] immediate_o;
+
+// to sequencer
+wire [`MICRO_ADDR_WIDTH-1:0] seq_addr_o;
+wire [3:0] src_o;
+wire [3:0] dst_o;
+wire [3:0] base_o;
+wire [3:0] index_o;
+wire [1:0] seg_o;
+
 wire decoded_valid_o;
 reg next_decoded_i;
 
@@ -61,38 +98,38 @@ zet_decode dut (
     .next_instruction_o(instruction_next),
 
     // current cs and ip of instruction
-    .instruction_cs_o(),
-    .instruction_ip_o(),
+    .instruction_cs_o(instruction_cs_o),
+    .instruction_ip_o(instruction_ip_o),
 
     // {prefix_decoded, repe_repz, repne_repnz, lock}
-    .inst_pr_o(),
+    .inst_pr_o(inst_pr_o),
     // {prefix_decoded, x, x, address size override}
-    .adrs_pr_o(),
+    .adrs_pr_o(adrs_pr_o),
     // {prefix_decoded, x, x, operand size override}
-    .oper_pr_o(),
+    .oper_pr_o(oper_pr_o),
     // {prefix_decoded, segment override register}
-    .sovr_pr_o(),
+    .sovr_pr_o(sovr_pr_o),
 
-    .opcode_o(),
+    .opcode_o(opcode_o),
 
-    .need_modrm_o(),
-    .modrm_o(),
+    .need_modrm_o(need_modrm_o),
+    .modrm_o(modrm_o),
 
-    .need_off_o(),
-    .off_size_o(),
-    .offset_o(),
+    .need_off_o(need_off_o),
+    .off_size_o(off_size_o),
+    .offset_o(offset_o),
 
-    .need_imm_o(),
-    .imm_size_o(),
-    .immediate_o(),
+    .need_imm_o(need_imm_o),
+    .imm_size_o(imm_size_o),
+    .immediate_o(immediate_o),
 
     // to sequencer
-    .seq_addr_o(),
-    .src_o(),
-    .dst_o(),
-    .base_o(),
-    .index_o(),
-    .seg_o(),
+    .seq_addr_o(seq_addr_o),
+    .src_o(src_o),
+    .dst_o(dst_o),
+    .base_o(base_o),
+    .index_o(index_o),
+    .seg_o(seg_o),
 
     .decoded_valid_o(decoded_valid_o),
     .next_decoded_i(next_decoded_i)
@@ -119,14 +156,46 @@ always @(posedge clk)
     instruction_valid <= 1'b1;
     if (instruction_next)
       begin
-        $display("Address: %d   Data byte: %h", count, instruction);
+        $display("Address: %h   data byte: %h", count, instruction);
         count <= count + 20'd1;
       end
     if (decoded_valid_o & !next_decoded_i)
       begin
-        $display("Instruction Finished Decoding");
+        $display("Instruction_cs: %h", instruction_cs_o);
+        $display("Instruction_ip: %h", instruction_ip_o);
         $display(" ");
-        $display("Sending next byte sequence:");
+        $display("Instruction prefix {prefix_decoded, repe_repz, repne_repnz, lock}: %b", inst_pr_o);
+        $display("Address Size Override prefix {prefix_decoded, x, x, address size override}: %b", adrs_pr_o);
+        $display("Operand Size Override prefix {prefix_decoded, x, x, operand size override}: %b", oper_pr_o);
+        $display("Segment Override prefix {prefix_decoded, segment override register}: %b", sovr_pr_o);
+        $display(" ");
+        $display("Opcode: %h", opcode_o);
+        $display(" ");
+        $display("Need Modrm: %b", need_modrm_o);
+        $display("Modrm: %h", modrm_o);
+        $display(" ");
+        $display("Need offset: %b",need_off_o);
+        $display("Offset Size: %b", off_size_o);
+        $display("Offset: %h", offset_o);
+        $display(" ");
+        $display("Need immediate: %b", need_imm_o);
+        $display("Immediate Size: %b", imm_size_o);
+        $display("Immediate: %h", immediate_o);
+        $display(" ");
+        $display("Sequencer Address in hex: %h, bit: %b", seq_addr_o, seq_addr_o);
+        $display("Sequencer Source in hex: %h, bit: %b", src_o, src_o);
+        $display("Sequencer Destination in hex: %h, bit: %b", dst_o, dst_o);
+        $display("Sequencer Base in hex: %h, bit: %b", base_o, base_o);
+        $display("Sequencer Index in hex: %h, bit: %b", index_o, index_o);
+        $display("Sequencer Segment in hex: %h, bit: %b", seg_o, seg_o);
+        $display(" ");
+        $display("Instruction finished decoding");
+        $display(" ");
+        $display(" ");
+        $display(" ");
+        $display(" ");
+        $display("########## Sending next byte sequence: ##########");
+        $display(" ");
         next_decoded_i <= 1'b1;
       end
     else
@@ -151,7 +220,8 @@ always begin
 	
 	$display(" ");
 	
-	$display("Sending first byte sequence: ");
+	$display("########## Sending first byte sequence: ##########");
+	$display(" ");
 		
 	#1000 rst <= 1'b1;
 		
